48c9ea8ebc52a5a6f76bf1f6873f4ac9e209b1be
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-07-03 03:46:03 +0530

    added stack navigation :100: :

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,35 @@ import { SafeAreaView, View } from "react-native";
 import Auth from "./screens/Auth";
 import { useCurrentUser } from "./hooks/useCurrentUser";
 import Core from "./screens/Core";
+import Collection from "./screens/Collection";
+import CoinDash from "./screens/CoinDash";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DegenCoinFlip from "./screens/DegenCoinFlip";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  // Determine whether user is logged in
-  // If logged in, display app core
-  // If not logged in, display auth screen
   const isLoggedIn = useCurrentUser()!!;
 
   return (
-    <View style={{ backgroundColor: "#f0f0f0" }}>
-      <SafeAreaView style={{ height: "100%" }}>
-        {isLoggedIn ? <Core /> : <Auth />}
-      </SafeAreaView>
-    </View>
+    <NavigationContainer>
+      <View style={{ backgroundColor: "#f0f0f0", flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Stack.Navigator>
+            {isLoggedIn ? (
+              <>
+                <Stack.Screen name="Core" component={Core} />
+                <Stack.Screen name="Collection" component={Collection} />
+                <Stack.Screen name="CoinDash" component={CoinDash} />
+                <Stack.Screen name="DegenCoinFlip" component={DegenCoinFlip} />
+              </>
+            ) : (
+              <Stack.Screen name="Auth" component={Auth} />
+            )}
+          </Stack.Navigator>
+        </SafeAreaView>
+      </View>
+    </NavigationContainer>
   );
 }
---
+++ src/screens/Collection.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const CollectionScreen = ({ navigation }) => {
+  const games = [
+    {
+      id: 1,
+      title: 'Doge n CoinFlip',
+      icon: '🎮',
+      onPress: () => navigation.navigate('DegenCoinFlip'),
+    },
+    {
+      id: 2,
+      title: 'Coin Dash',
+      icon: '🕹️',
+      onPress: () => navigation.navigate('CoinDash'),
+    },
+    { id: 3, title: 'Game 3', icon: '🎯' },
+    // Add more games as needed
+  ];
+
+  return (
+    <View style={styles.container}>
+      {games.map((game) => (
+        <TouchableOpacity key={game.id} style={styles.gameItem} onPress={game.onPress}>
+          <Text style={styles.gameIcon}>{game.icon}</Text>
+          <Text style={styles.gameTitle}>{game.title}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gameItem: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  gameIcon: {
+    fontSize: 40,
+  },
+  gameTitle: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default CollectionScreen;
